e80406238c3cce292b38ae59590695d12082665c
Gui2885 <00001080881074sp@al.educacao.sp.gov.br>
2024-10-29 09:20:16 -0300

    flashcards

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Rock in Roll',
+    'Quem é conhecido como "O Rei do Rock"?',
+    'Elvis Presley é amplamente conhecido como "O Rei do Rock". Ele ajudou a popularizar o gênero nos anos 1950 e deixou um legado musical que impacta gerações até hoje.'
+)
+
+criaCartao(
+    'Rock in Roll',
+    'Qual banda britânica é considerada uma das maiores do rock e é famosa por músicas como "Bohemian Rhapsody" e "We Will Rock You"?',
+    'A banda Queen, formada em 1970, é uma das maiores do rock. Com Freddie Mercury como vocalista, Brian May na guitarra, John Deacon no baixo e Roger Taylor na bateria, o Queen é famoso por seu estilo único e inovação no rock.'
+)
+
+criaCartao(
+    'Rock in Roll',
+    'Qual foi o principal movimento de rock nos anos 1990, caracterizado por bandas como Nirvana e Pearl Jam?',
+    'Esse movimento foi o Grunge, originado em Seattle, nos Estados Unidos. Caracterizado por letras introspectivas e um som pesado e cru, o grunge teve grande influência no rock da época, com Nirvana e Pearl Jam como principais expoentes.'
+)
+
+criaCartao(
+    'Rock in Roll',
+    'Qual é o nome do guitarrista conhecido pelo uso icônico da guitarra invertida e canhota, e por tocar com os dentes em apresentações?',
+    'Jimi Hendrix é conhecido por seu estilo único de tocar guitarra, incluindo sua técnica inovadora e performances explosivas nos palcos. Ele influenciou gerações de guitarristas com seu talento e criatividade incomparáveis.'
+)
